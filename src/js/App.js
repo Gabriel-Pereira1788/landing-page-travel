220601dbb35b9b2901baa6b8182e093497bf2b8d
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2022-06-24 22:45:38 -0300

    Modificando pasta de local

--- src/js/App.js
+++ src/js/App.js
@@ -6,7 +6,7 @@ const contentMain = document.querySelector(".container-main");
 const containerImages = document.querySelector(".container-imgs");
 const imgTravel = containerImages.querySelectorAll(".img-travel");
 
-const urlData = "../../src/data/Data.json";
+const urlData = "../../data/Data.json";
 const comments = document.querySelectorAll(".comment");
 
 const getData = async (fn) => {
