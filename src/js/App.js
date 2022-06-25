8c9dd0888a6ff2502afbdad0238b6de8576a7d54
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2022-06-24 22:35:58 -0300

    .

--- src/js/App.js
+++ src/js/App.js
@@ -11,12 +11,12 @@ const comments = document.querySelectorAll(".comment");
 
 const getData = async (fn) => {
 	try {
-		const response = fetch(urlData);
-		const data = response.json();
-		fn(data);
+		const response = await fetch(urlData);
+		const data = await response.json();
+		fn(data)
 		if(!response.ok){
 			throw new Error('Erro encontrado')
-			
+
 		}
 	} catch (err) {
 		console.log(err);
