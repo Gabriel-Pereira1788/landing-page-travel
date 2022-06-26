15d3e0228167fb9d5e8f2fc0a42d3659f22d8a25
Gabriel-Pereira1788 <gabriel.pereira1788@gmail.com>
2022-06-26 17:05:59 -0300

    Ajustando reload com click da logo

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,5 @@
 const containerTitle = document.querySelector(".container-headers");
+const logoImg = document.querySelector('.logo')
 const containerSocialMedias = document.querySelector(".social-medias");
 const contentMain = document.querySelector(".container-main");
 const containerImages = document.querySelector(".container-imgs");
@@ -20,7 +21,7 @@ const menuIcon = document.querySelector(".menu-icon");
 const exitMenu = document.querySelector(".exit-menu");
 menuIcon.addEventListener("click", displayToggle);
 exitMenu.addEventListener("click", displayToggle);
-
+logoImg.addEventListener('click', () => document.location.reload(true))
 export default {
 	containerSocialMedias,
 	containerTitle,
